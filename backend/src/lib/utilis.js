1261a9c763a5aef755be425e2c267e9d525df10d
1Rohannaik <rohannaik@gmail.com>
2025-07-07 15:28:20 +0530

    Resolved merge conflict in utilis.js and updated backend

--- backend/src/lib/utilis.js
+++ backend/src/lib/utilis.js
@@ -8,8 +8,8 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: "None",   // Required for cross-site cookie in production
-    secure: true,       // Must be true in production (HTTPS)
+    sameSite: "None", // Required for cross-site cookie in production
+    secure: true, // Must be true in production (HTTPS)
   });
 
   return token;
