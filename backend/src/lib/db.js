7a72408a987211abe85c8de13f600fe8834c30d0
Rohan Naik <139874770+1Rohannaik@users.noreply.github.com>
2025-07-07 15:12:48 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT || "mysql",
+    dialect: process.env.DB_DIALECT || "postgres",
     dialectOptions: {
       ssl: {
         require: true,
